3a19d2d11fd99b6bda062936af104102992857b9
natalia.sepka <natalia.sepka@gmail.com>
2024-03-13 15:46:44 +0100

    Displaying blog posts.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'natalia', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'natalia', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'natalia', id: 3 }
+    ]);
     return (
         <div className="home">
-            <h1>Home Page</h1>
+        { blogs.map((blog) => (
+        <div className="blog-preview" key={ blog.id }>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+        </div>
+        )) }
         </div>
     );
 }
