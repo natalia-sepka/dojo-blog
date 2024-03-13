6cc20441683b4eda20a25fa88ef84cbab802fa64
natalia.sepka <natalia.sepka@gmail.com>
2024-03-13 16:58:45 +0100

    Handling deleting articles.

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'natalia', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'natalia', id: 2 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'lukasz', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'natalia', id: 3 }
     ]);
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    };
     return (
         <div className="home">
-        { blogs.map((blog) => (
-        <div className="blog-preview" key={ blog.id }>
-            <h2>{ blog.title }</h2>
-            <p>Written by { blog.author }</p>
-        </div>
-        )) }
+        <BlogList blogs={blogs} title='All blogs!' handleDelete={handleDelete} />
         </div>
     );
 }
